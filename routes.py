dd8bb8375eea5a668b20403fb0add5084811c72a
Chinonso Anyanka <cc792@njit.edu>
2021-08-02 10:29:20 -0400

    Part 3 of tutorial

--- routes.py
+++ routes.py
@@ -1,19 +1,24 @@
-"""Route declaration."""
-from flask import current_app as app
-from flask import render_template
+from flask import (
+    Flask,
+    url_for,
+    render_template,
+    redirect
+)
+from .forms import ContactForm
 
 
-@app.route("/")
-def home():
-    """Landing page route."""
-    nav = [
-        {"name": "Home", "url": "https://example.com/1"},
-        {"name": "About", "url": "https://example.com/2"},
-        {"name": "Pics", "url": "https://example.com/3"},
-    ]
+app = Flask(__name__, instance_relative_config=False)
+app.config.from_object('config.Config')
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    """Standard `contact` form."""
+    form = ContactForm()
+    if form.validate_on_submit():
+        return redirect(url_for("success"))
     return render_template(
-        "home.html",
-        nav=nav,
-        title="Jinja Demo Site",
-        description="Smarter page templates with Flask & Jinja.",
+        "contact.jinja2",
+        form=form,
+        template="form-template"
     )
